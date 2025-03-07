c96f4d5f3f89fe0220df5ac1c51039e5ea6abb47
Pushpendra Jakhar <pjakhar@iitb.ac.in>
2025-03-07 12:12:38 +0530

    tested image gen

--- testGemini.py
+++ testGemini.py
@@ -1,16 +1,34 @@
 from google import genai
+API_KEY = "AIzaSyC0eDOtjtV52oEa3oogHRhrQkHOf5abdMs"
+client = genai.Client(api_key=API_KEY)
 
-client = genai.Client(api_key="AIzaSyC0eDOtjtV52oEa3oogHRhrQkHOf5abdMs")
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=["Explain how AI works"],
+)
 
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash",
-#     contents=["Explain how AI works"],
-# )
-
-for chunk in client.models.generate_content_stream(
-        model='gemini-2.0-flash',
-        contents='''What is modern family?'''
-      ):
-        print(chunk.text)
+# for chunk in client.models.generate_content_stream(
+#         model='gemini-2.0-flash',
+#         contents='''What is modern family?'''
+#       ):
+#         print(chunk.text)
 
 # print(response.text)
+
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+
+client = genai.Client(api_key=API_KEY)
+
+# response = client.models.generate_images(
+#     model='imagen-3.0-generate-002',
+#     prompt='Fuzzy bunnies in my kitchen',
+#     config=types.GenerateImagesConfig(
+#         number_of_images= 4,
+#     )
+# )
+# for generated_image in response.generated_images:
+#   image = Image.open(BytesIO(generated_image.image.image_bytes))
+#   image.show()
